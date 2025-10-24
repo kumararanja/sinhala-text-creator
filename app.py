c2d529e5b072a3029b9680329a4c11ebb3691d29
kumararanja <69043199+kumararanja@users.noreply.github.com>
2025-10-24 20:33:49 +1100

    port 8000 updated

--- app.py
+++ app.py
@@ -1352,4 +1352,5 @@ if __name__ == "__main__":
     print("=" * 50)
     
     demo = create_interface()
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    demo.launch(server_name="0.0.0.0", server_port=8000)
+
